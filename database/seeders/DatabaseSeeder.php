21638f0a624588e2ad444783ef22f3bed623faa3
mawena <gamligocharles@gmail.com>
2024-06-27 11:42:24 +0000

    fix bug migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,29 +29,29 @@ class DatabaseSeeder extends Seeder
 		$user = False;
 		$vt = False;
 
-		if(!$user){
+		if (!$user) {
 			DB::unprepared(file_get_contents("./database/users.sql"));
 		}
 
-		if($user){
+		if ($user) {
 			$admin = User::factory(1)->create(["name" => "admin", "full_name" => "admin", "profile" => "admin", "password" => "Coftg@20$*21ù!ad", "password_change_required" => false, "activated" => true, "email" => "admin@cofinacorp.com"])->first();
-	
+
 			$credit_analyst = User::factory(1)->create(["name" => "credit_analyst", "full_name" => "Credit Analyst", "profile" => "credit_analyst", "password" => "P@sse123", "password_change_required" => false, "activated" => true, "email" => "credit_analyst@cofinacorp.com"])->first();
 			$credit_analyst2 = User::factory(1)->create(["name" => "credit_analyst2", "full_name" => "Credit Analyst 2", "profile" => "credit_analyst", "password" => "P@sse123", "password_change_required" => false, "activated" => true, "email" => "credit_analyst2@cofinacorp.com"])->first();
 			$credit_analyst3 = User::factory(1)->create(["name" => "credit_analyst3", "full_name" => "Credit Analyst 3", "profile" => "credit_analyst", "password" => "P@sse123", "password_change_required" => false, "activated" => true, "email" => "credit_analyst3@cofinacorp.com"])->first();
-	
+
 			$credit_admin = User::factory(1)->create(["name" => "credit_admin", "full_name" => "Credit Admin", "profile" => "credit_admin", "password" => "P@sse123", "password_change_required" => false, "activated" => true, "email" => "credit_admin@cofinacorp.com"])->first();
 			$credit_admin2 = User::factory(1)->create(["name" => "credit_admin2", "full_name" => "Credit Admin 2", "profile" => "credit_admin", "password" => "P@sse123", "password_change_required" => false, "activated" => true, "email" => "credit_admin2@cofinacorp.com"])->first();
 			$credit_admin3 = User::factory(1)->create(["name" => "credit_admin3", "full_name" => "Credit Admin 3", "profile" => "credit_admin", "password" => "P@sse123", "password_change_required" => false, "activated" => true, "email" => "credit_admin3@cofinacorp.com"])->first();
-	
+
 			$head_credit = User::factory(1)->create(["name" => "head_credit", "full_name" => "Head Credit", "profile" => "head_credit", "password" => "P@sse123", "password_change_required" => false, "activated" => true, "email" => "head_credit@cofinacorp.com"])->first();
-	
+
 			$operation = User::factory(1)->create(["name" => "operation", "full_name" => "Operation", "profile" => "operation", "password" => "P@sse123", "password_change_required" => false, "activated" => true, "email" => "operation@cofinacorp.com"])->first();
-	
+
 			$legal = User::factory(1)->create(["name" => "legal", "full_name" => "Legal", "profile" => "legal", "password" => "P@sse123", "password_change_required" => false, "activated" => true, "email" => "legal@cofinacorp.com"])->first();
-	
+
 			$dex = User::factory(1)->create(["name" => "dex", "full_name" => "Dex", "profile" => "dex", "password" => "P@sse123", "password_change_required" => false, "activated" => true, "email" => "dex@cofinacorp.com"])->first();
-	
+
 			// foreach ([["full_name" => "Yao HOETOWOU", "email" => "yao.hoetowou@cofinacorp.com"], ["full_name" => "Simen Broklyn MEBA", "email" => "simen.meba@cofinacorp.com"], ["full_name" => "Nyokpogbe AGBENOWOSI", "email" => "nykpogbe.agbenowosi@cofinacorp.com"], ["full_name" => "Mawule Kokou DOGBE", "email" => "kokou.dogbe@cofinacorp.com"], ["full_name" => "Kossi AGBLEVON", "email" => "kossi.agblevon@cofinacorp.com"], ["full_name" => "Kossi Dodji AMEGBEDJI", "email" => "dodji.amegbedji@cofinacorp.com"], ["full_name" => "Kosi Mensa DUYIBOE", "email" => "kosi-mensa.duyiboe@cofinacorp.com"], ["full_name" => "Komlan Somefa AHATEFOU", "email" => "somefa.ahatefou@cofinacorp.com"], ["full_name" => "Komlan DADZIE", "email" => "komlan.dadzie@cofinacorp.com"], ["full_name" => "Komla Nopeli AHONDE", "email" => "komla.ahonde@cofinacorp.com"], ["full_name" => "Komi Vienyeawu KUNAKEY", "email" => "komi.kunakey@cofinacorp.com"], ["full_name" => "Komi Elom ZAGARAGO", "email" => "elom.zagarago@cofinacorp.com"], ["full_name" => "Komi Dogbéda NOLI", "email" => "komi.noli@cofinacorp.com"], ["full_name" => "Koffivi Amenyo TOLESSI", "email" => "koffivi.tolessi@cofinacorp.com"], ["full_name" => "Koffi WOEKPO", "email" => "koffi.woekpo@cofinacorp.com"], ["full_name" => "Kadiko Pyalo SOHOU", "email" => "kadiko-pyalo.sohou@cofinacorp.com"], ["full_name" => "Joseph AGBI", "email" => "joseph.agbi@cofinacorp.com"], ["full_name" => "Gnonyarou WALLA", "email" => "gnonyarou.walla@cofinacorp.com"], ["full_name" => "Gianni Patrick Attiogbe KOUDOSSOU", "email" => "gianni.koudossou@cofinacorp.com"], ["full_name" => "Gafar Tchapo NABINE", "email" => "gafar.nabine@cofinacorp.com"], ["full_name" => "Francis K GAVISSE", "email" => "francis.gavisse@cofinacorp.com"], ["full_name" => "Fatima Badoawè TCHA-COROUDOU", "email" => "fatima.tcha-coroudou@cofinacorp.com"], ["full_name" => "David Martial SOUSSOUKPO", "email" => "david-martial.soussoukpo@cofinacorp.com"], ["full_name" => "Chimene AZEGUE", "email" => "chimene.azegue@cofinacorp.com"], ["full_name" => "Baliza TEKPEZI", "email" => "baliza.tekpezi@cofinacorp.com"], ["full_name" => "Assowè MABOUGRE", "email" => "assowe.mabougre@cofinacorp.com"], ["full_name" => "Améyo Chimène Edwige ATTILA", "email" => "edwige.attila@cofinacorp.com"], ["full_name" => "Amah AYIKOE", "email" => "amah.ayikoe@cofinacorp.com"], ["full_name" => "Aimé Edem HOUNSOU DEGBE", "email" => "aime.hounsou-degbe@cofinacorp.com"], ["full_name" => "Abra DOUGAME", "email" => "abra.dougame@cofinacorp.com"], ["full_name" => "Ablam Samuel AKAKPO", "email" => "samuel.akakpo@cofinacorp.com"], ["full_name" => "Abire KPENIFEI KPATCHA", "email" => "abire.kpenifei@cofinacorp.com"], ["full_name" => "Abdou Kabirou YOMENOU", "email" => "kabirou.yomenou@cofinacorp.com"],] as $userData) {
 			// 	$elements = explode("@cofinacorp.com", $userData["email"]);
 			// 	$caf = User::factory(1)->create(["name" => strtolower($elements[0]), "full_name" => $userData["full_name"], "profile" => "caf", "password" => "P@sse123", "password_change_required" => false, "activated" => true, "email" => $userData["email"]])->first();
@@ -59,11 +59,11 @@ class DatabaseSeeder extends Seeder
 			$caf = User::factory(1)->create(["name" => strtolower("caf"), "full_name" => "CAF", "profile" => "caf", "password" => "P@sse123", "password_change_required" => false, "activated" => true, "email" => "caf@cofinacorp.com"])->first();
 			$caf2 = User::factory(1)->create(["name" => strtolower("caf2"), "full_name" => "CAF 2", "profile" => "caf", "password" => "P@sse123", "password_change_required" => false, "activated" => true, "email" => "caf2@cofinacorp.com"])->first();
 			$caf3 = User::factory(1)->create(["name" => strtolower("caf3"), "full_name" => "CAF 3", "profile" => "caf", "password" => "P@sse123", "password_change_required" => false, "activated" => true, "email" => "caf3@cofinacorp.com"])->first();
-			
+
 			$ca = User::factory(1)->create(["name" => "ca", "full_name" => "CA", "profile" => "ca", "password" => "P@sse123", "password_change_required" => false, "activated" => true, "email" => "ca@cofinacorp.com"])->first();
-	
+
 			$md = User::factory(1)->create(["name" => "md", "full_name" => "MD", "profile" => "md", "password" => "P@sse123", "password_change_required" => false, "activated" => true, "email" => "md@cofinacorp.com"])->first();
-		
+
 			if ($vt) {
 				VerbalTrial::factory(5)->create(["creator_id" => $credit_analyst->id, "status" => "validated", "credit_admin_id" => $credit_admin->id])->each(function ($verbalTrial) use ($credit_admin) {
 					Guarantee::factory(5)->create(["verbal_trial_id" => $verbalTrial->id]);
@@ -87,7 +87,7 @@ class DatabaseSeeder extends Seeder
 						Guarantor::factory(3)->create(["contract_id" => $contract->id]);
 					});
 				});
-	
+
 				//Pv avec contrat et avec CAT
 				VerbalTrial::factory(5)->create(["creator_id" => $credit_analyst->id, "credit_admin_id" => $credit_admin->id])->each(function ($verbalTrial) use ($credit_admin) {
 					Guarantee::factory(5)->create(["verbal_trial_id" => $verbalTrial->id]);
@@ -96,12 +96,12 @@ class DatabaseSeeder extends Seeder
 						CAT::factory(1)->create(["contract_id" => $contract->id]);
 					});
 				});
-	
+
 				//Pv sans contrat
 				VerbalTrial::factory(15)->create(["creator_id" => $credit_analyst->id, "credit_admin_id" => $credit_admin->id])->each(function ($verbalTrial) {
 					Guarantee::factory(5)->create(["verbal_trial_id" => $verbalTrial->id]);
 				});
-	
+
 				//Pv hypothécaire non validé
 				VerbalTrial::factory(5)->create(["creator_id" => $credit_analyst->id, "credit_admin_id" => $credit_admin->id])->each(function ($verbalTrial) {
 					Guarantee::factory(2)->create(["verbal_trial_id" => $verbalTrial->id, "type_of_guarantee_id" => 9]);
@@ -111,6 +111,26 @@ class DatabaseSeeder extends Seeder
 					Guarantee::factory(2)->create(["verbal_trial_id" => $verbalTrial->id, "type_of_guarantee_id" => 9]);
 				});
 			}
+
+			$plainTextToken = $admin->createToken("auth-token")->plainTextToken;
+			DB::update("update personal_access_tokens set TOKEN = '8fb55a1d50842403ddc4ea7dc0c80a5d2e44eeb029f1077341babd46b68fe0ba' where ID = 1");
+			$plainTextToken = $admin->createToken("auth-token")->plainTextToken;
+			DB::update("update personal_access_tokens set TOKEN = '96b91ec21dfc7acd2bb8489528e67dbabaf8d6da488267f72069ebb0f09658d6' where ID = 2");
+			$plainTextToken = "1|c96jDUWBogbtZRsU6Oo9ZbzL3ZB5ry2spd3PC5RHd9464644";
+			$file = public_path('../.env');
+			$lines = file($file);
+
+			if (count($lines) >= 1) {
+				$lines[count($lines) - 1] = "SCRIBE_AUTH_KEY=$plainTextToken\n";
+
+				// Réécrire le contenu modifié dans le fichier
+				file_put_contents($file, implode('', $lines));
+			} else {
+				// Gérer le cas où le fichier n'a pas assez de lignes
+				echo "Le fichier ne contient pas suffisamment de lignes pour effectuer le remplacement.";
+			}
+
+			echo "admin Token: " . $plainTextToken . "\n";
 
 		}
 
@@ -155,27 +175,7 @@ class DatabaseSeeder extends Seeder
 			TypeOfGuarantee::factory(1)->create(["name" => $typeOfGuaranteeName]);
 		}
 
-		
 
-		$plainTextToken = $credit_admin->createToken("auth-token")->plainTextToken;
-		DB::update("update personal_access_tokens set TOKEN = '8fb55a1d50842403ddc4ea7dc0c80a5d2e44eeb029f1077341babd46b68fe0ba' where ID = 1");
-		$plainTextToken = $credit_admin->createToken("auth-token")->plainTextToken;
-		DB::update("update personal_access_tokens set TOKEN = '96b91ec21dfc7acd2bb8489528e67dbabaf8d6da488267f72069ebb0f09658d6' where ID = 2");
-		$plainTextToken = "1|c96jDUWBogbtZRsU6Oo9ZbzL3ZB5ry2spd3PC5RHd9464644";
-		$file = public_path('../.env');
-		$lines = file($file);
-
-		if (count($lines) >= 1) {
-			$lines[count($lines) - 1] = "SCRIBE_AUTH_KEY=$plainTextToken\n";
-
-			// Réécrire le contenu modifié dans le fichier
-			file_put_contents($file, implode('', $lines));
-		} else {
-			// Gérer le cas où le fichier n'a pas assez de lignes
-			echo "Le fichier ne contient pas suffisamment de lignes pour effectuer le remplacement.";
-		}
-
-		echo "admin Token: " . $plainTextToken . "\n";
 
 
 	}
