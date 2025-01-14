474c784a3da1ac6fc62cba23e784703274a7b44f
mawena <gamligocharles@gmail.com>
2025-01-14 15:08:00 +0000

    sendEmail

--- app/Jobs/SendEmail.php
+++ app/Jobs/SendEmail.php
@@ -35,7 +35,7 @@ class SendEmail implements ShouldQueue
     public function handle(): void
     {
         try {
-            Mail::to("gamligocharles2@gmail.com")->send(
+            Mail::to($this->receiverEmail)->send(
                 new EmailSkeleton(
                     $this->subject,
                     $this->content
