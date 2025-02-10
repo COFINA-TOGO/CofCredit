5e6373eab07b5401e912c5d00d015c19b67cb826
mawena <gamligocharles@gmail.com>
2025-02-10 11:52:38 +0000

    fix bug

--- app/Http/Controllers/API/ContractController.php
+++ app/Http/Controllers/API/ContractController.php
@@ -914,46 +914,49 @@ class ContractController extends Controller
 					$mailsDataList = [
 						"validated" =>
 						[
-							"receiverList" => $receiverList["credit_admin_list"],
-							"subject" => "Notification de validation du PV " . $contract->verbal_trial->committee_id,
-							"message" => "
-										<h1 style='color: #333333;font-size: 24px; margin-bottom: 20px;'>Cher(e) Admin crédit,</U></h1>
-
-										<p style='color: #666666; font-size: 16px; line-height: 1.5;'>Nous vous prions de vous connecter à l'application cofina credit digital et de prendre en charge immédiatement le contrat" . $contract->verbal_trial->committee_id . "en attente de cat: <a href='" . env("APP_URL") . "/cat/add?id=" . $contract->id . "'>Créer le cat</a></p>
-
-										<p style='color: #666666; font-size: 16px; line-height: 1.5;'>Si vous avez des questions ou des préoccupations, n'hésitez pas à nous contacter. Nous sommes là pour vous aider !</p>
-
-										<hr style='border: none; border-top: 1px solid #dddddd; margin: 20px 0;'>
-
-										<p style='color: #999999; font-size: 12px;'>Cet e-mail est généré automatiquement. Veuillez ne pas y répondre.</p>
-									",
+							[
+								"receiverList" => $receiverList["credit_admin_list"],
+								"subject" => "Notification de validation du contrat " . $contract->verbal_trial->committee_id,
+								"message" => "
+											<h1 style='color: #333333;font-size: 24px; margin-bottom: 20px;'>Cher(e) Admin crédit,</U></h1>
+	
+											<p style='color: #666666; font-size: 16px; line-height: 1.5;'>Nous vous prions de vous connecter à l'application cofina credit digital et de prendre en charge immédiatement le contrat" . $contract->verbal_trial->committee_id . "en attente de cat: <a href='" . env("APP_URL") . "/cat/add?id=" . $contract->id . "'>Créer le cat</a></p>
+	
+											<p style='color: #666666; font-size: 16px; line-height: 1.5;'>Si vous avez des questions ou des préoccupations, n'hésitez pas à nous contacter. Nous sommes là pour vous aider !</p>
+	
+											<hr style='border: none; border-top: 1px solid #dddddd; margin: 20px 0;'>
+	
+											<p style='color: #999999; font-size: 12px;'>Cet e-mail est généré automatiquement. Veuillez ne pas y répondre.</p>
+										",
+							]
 						],
 						"rejected" =>
 						[
-
-							"receiverList" => $receiverList["caf_list"],
-							"subject" => "Notifcation de rejet du PV " . $contract->verbal_trial->committee_id,
-							"message" => "
-										<h1 style='color: #333333; font-size: 24px; margin-bottom: 20px;'>Cher(e) Analyste Crédit,</h1>
-
-										<p style='color: #666666; font-size: 16px; line-height: 1.5;'>
-										Nous vous informons que le contrat <strong>" . $contract->verbal_trial->committee_id . "</strong> a été rejeté lors de sa validation. Nous vous invitons à vous connecter à l'application Cofina Crédit Digital pour consulter les motifs de rejet et effectuer les actions nécessaires.
-										</p>
-
-										<p style='color: #666666; font-size: 16px; line-height: 1.5;'>
-										Pour accéder directement aux contrats, cliquez sur le lien suivant : <a href='#'>Voir les contrats</a>.
-										</p>
-
-										<p style='color: #666666; font-size: 16px; line-height: 1.5;'>
-										Si vous avez des questions ou des préoccupations, n'hésitez pas à nous contacter. Nous restons à votre disposition pour toute assistance !
-										</p>
-
-										<hr style='border: none; border-top: 1px solid #dddddd; margin: 20px 0;'>
-
-										<p style='color: #999999; font-size: 12px;'>
-										Cet e-mail est généré automatiquement. Veuillez ne pas y répondre.
-										</p>
-									",
+							[
+								"receiverList" => $receiverList["caf_list"],
+								"subject" => "Notifcation de rejet du PV " . $contract->verbal_trial->committee_id,
+								"message" => "
+											<h1 style='color: #333333; font-size: 24px; margin-bottom: 20px;'>Cher(e) Analyste Crédit,</h1>
+	
+											<p style='color: #666666; font-size: 16px; line-height: 1.5;'>
+											Nous vous informons que le contrat <strong>" . $contract->verbal_trial->committee_id . "</strong> a été rejeté lors de sa validation. Nous vous invitons à vous connecter à l'application Cofina Crédit Digital pour consulter les motifs de rejet et effectuer les actions nécessaires.
+											</p>
+	
+											<p style='color: #666666; font-size: 16px; line-height: 1.5;'>
+											Pour accéder directement aux contrats, cliquez sur le lien suivant : <a href='#'>Voir les contrats</a>.
+											</p>
+	
+											<p style='color: #666666; font-size: 16px; line-height: 1.5;'>
+											Si vous avez des questions ou des préoccupations, n'hésitez pas à nous contacter. Nous restons à votre disposition pour toute assistance !
+											</p>
+	
+											<hr style='border: none; border-top: 1px solid #dddddd; margin: 20px 0;'>
+	
+											<p style='color: #999999; font-size: 12px;'>
+											Cet e-mail est généré automatiquement. Veuillez ne pas y répondre.
+											</p>
+										",
+							]
 						],
 					];
 					$mailsData = $mailsDataList[$requestData["status"]];
